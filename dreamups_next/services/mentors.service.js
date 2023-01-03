4720dea149b6a097ead41d37c7e505da37198c39
MaxCojocari <cojocarigoncearmax@gmail.com>
2023-01-03 22:38:36 +0200

    add axiosAuthHeader

--- dreamups_next/services/mentors.service.js
+++ dreamups_next/services/mentors.service.js
@@ -3,40 +3,47 @@ import { API_URL } from '../config/config';
 import { axiosAuthHeader } from "../config/axios.config";
 
 export const getFixedNrMentors = () => {
-    return axios.get(`${API_URL}/mentors/limit`).then((res) => {
-        return res;
-    }).catch(e => console.log(e))
+  return axios.get(`${API_URL}/mentors/limit`).then((res) => {
+    return res;
+  }).catch(e => console.log(e))
 }
 
 export const getAllMentors = async () => {
-
-    return axios.get(`${API_URL}/mentors/all`, await axiosAuthHeader()).then((res) => {
-        return res;
-    }).catch(e => console.log(e))
+  return axios.get(`${API_URL}/mentors/all`, await axiosAuthHeader()).then((res) => {
+    return res;
+  }).catch(e => console.log(e))
 }
 
 
 export const searchMentors = async (tagsArray) => {
-    // post?
-    return axios.post(`${API_URL}/mentors/search`, { tags: tagsArray }).then((res) => {
-        return res;
-    }).catch(e => console.log(e))
+  return axios.post(
+    `${API_URL}/mentors/search`,
+    { tags: tagsArray },
+    await axiosAuthHeader()
+  ).then((res) => {
+    return res;
+  }).catch(e => console.log(e))
 }
-
 
 
 export const searchQueryMentors = async (tagsArray) => {
-
-    return axios.post(`${API_URL}/mentors/search?tag=${tagsArray.join(',')}`, tagsArray).then((res) => {
-        return res;
-    }).catch(e => console.log(e))
+  return axios.post(
+    `${API_URL}/mentors/search?tag=${tagsArray.join(',')}`,
+    tagsArray,
+    await axiosAuthHeader()
+  ).then((res) => {
+    return res;
+  }).catch(e => console.log(e))
 }
 
-export const getMentorById = async (id) => {
 
-    return axios.get(`${API_URL}/mentors?id=${id}`).then((res) => {
-        return res;
-    }).catch(e => console.log(e))
+export const getMentorById = async (id) => {
+  return axios.get(
+    `${API_URL}/mentors?id=${id}`,
+    await axiosAuthHeader()
+  ).then((res) => {
+    return res;
+  }).catch(e => console.log(e))
 }
 
 
