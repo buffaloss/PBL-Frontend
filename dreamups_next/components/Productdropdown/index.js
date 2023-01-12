a456bb6112d32cfd98058c9657a577a231c84f93
Razus228 <cassilasq2@gmail.com>
2023-01-12 16:40:56 +0200

    first part product

--- dreamups_next/components/Productdropdown/index.js
+++ dreamups_next/components/Productdropdown/index.js
@@ -1,17 +1,59 @@
 import React from "react";
-import { Dropdown } from "@nextui-org/react";
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { ProductItem } from "./styles";
 
 
 
 export default function Product(){
     return(
-        <Dropdown>
-            <Dropdown.Button light>
-                <ProductItem>
-                    Product
-                </ProductItem>
-            </Dropdown.Button>
-        </Dropdown>
+        <div>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger className="trigger" >
+                    <ProductItem>
+                        Product
+                    </ProductItem>
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content className="content">
+                    <DropdownMenu.Item className="color header" disabled>
+                        Accelerators and Schools
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="item">
+                        Startup School
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        Dreamable
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        Upcelerator
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="align">
+                        UpNext
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="color" disabled>
+                        Events
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="item">
+                        Catalyst Hackathons
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        Dreamicon
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item>
+                        Startup Grind Chișinău
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="align">
+                        Founders Network
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="color" disabled>
+                        Smart Capital
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="item align">
+                        Mozaic
+                    </DropdownMenu.Item>
+                   
+
+                </DropdownMenu.Content>
+            </DropdownMenu.Root>
+        </div>
     )
 }
